4b164a527f6720f8c67f32aa1e425bfd00f445a8
Mateo-Bedoya <mb291959@gmail.com>
2025-04-24 21:34:30 -0400

    new: creates init method in the board class, dimensions of board/user diffuculty

--- board.py
+++ board.py
@@ -1,4 +1,34 @@
+from pygame import *
+from sudoku_generator import generate_sudoku
+
 class Board:
 
-    def __init__(self):
+    def __init__(self, width, height, screen, difficulty):
+
+        self.screen = screen
+        self.difficulty = difficulty
+        self.width = width
+        self.height = height
+
+        if difficulty == "easy":
+            removed = 30
+        elif difficulty == "medium":
+            removed = 40
+        else:
+            removed = 50
+
+        self.board_values = generate_sudoku(9,removed)
+
+        self.cells = []
+
+        for row in range(9):
+            self.cells.append([])
+            for col in range(9):
+                values = self.board_values[row][col]
+                cell = Cell(values,row,col,screen)
+                self.cells[row].append(cell)
+
+        self.selected = None
+
+    def draw(self):
         pass
