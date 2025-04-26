a09b679b7e2e2f31d313dd87a254f7ac37f77647
OvidioCalvet <calvetovidio@gmail.com>
2025-04-26 15:03:53 -0400

    creates the rest of the screens that will be implemented in the final flow of the user experience

--- sudoku.py
+++ sudoku.py
@@ -1,5 +1,5 @@
 # Imports
-from sudoku_generator import SudokuGenerator, generate_sudoku
+from sudoku_generator import SudokuGenerator
 from board import Board
 from cell import Cell
 import pygame, sys
@@ -40,14 +40,111 @@ def draw_welcome_screen(screen):
     hard_button_container = hard_button.get_rect(center = (WIDTH // 2, HEIGHT // 2 + 150))
     screen.blit(hard_button, hard_button_container)
 
-def draw_game_screen():
-    pass
+    while True:
 
-def draw_win_screen():
-    pass
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if easy_button_container.collidepoint(event.pos):
+                    return "easy"
+                elif medium_button_container.collidepoint(event.pos):
+                    return "medium"
+                elif hard_button_container.collidepoint(event.pos):
+                    return "hard"
+                
+        pygame.display.update()
 
-def draw_lose_screen():
-    pass
+def draw_game_screen(screen, difficulty):
+    # Innitialize Fonts
+    button_font = pygame.font.Font(None, 70)
+
+    board = Board(width=9, height=9, screen=screen, difficulty=difficulty)
+
+    # Draw the "reset" button surface
+    reset_button = button_font.render("RESET", 0, TEXT_COLOR)
+    reset_button_container = reset_button.get_rect(center = (WIDTH // 2 - 300, HEIGHT // 2 + 350))
+    screen.blit(reset_button, reset_button_container)
+
+    # Draw the "restart" button surface
+    restart_button = button_font.render("RESTART", 0, TEXT_COLOR)
+    restart_button_container = restart_button.get_rect(center = (WIDTH // 2, HEIGHT // 2 + 350))
+    screen.blit(restart_button, restart_button_container)
+
+    # Draw the "exit" button surface
+    exit_button = button_font.render("EXIT", 0, TEXT_COLOR)
+    exit_button_container = exit_button.get_rect(center = (WIDTH // 2 + 300, HEIGHT // 2 + 350))
+    screen.blit(exit_button, exit_button_container)
+
+    while True:
+
+        for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    if exit_button_container.collidepoint(event.pos):
+                        sys.exit()
+                    elif reset_button_container.collidepoint(event.pos):
+                        return "win"
+                    elif restart_button_container.collidepoint(event.pos):
+                        return "lose"
+        pygame.display.update()
+
+def draw_win_screen(screen):
+    # Initialize fonts
+    title_font = pygame.font.Font(None, 200)
+    button_font = pygame.font.Font(None, 70)
+    
+    # Sets the screens background color to custom pallete
+    screen.fill(BACKGROUND_COLOR)
+
+    # Draw the title surface
+    title = title_font.render("YOU WON!", 0, TEXT_COLOR)
+    title_container = title.get_rect(center = (WIDTH // 2, HEIGHT // 2 - 150))
+    screen.blit(title, title_container)
+
+    # Draw the "exit" button surface
+    exit_button = button_font.render("EXIT", 0, TEXT_COLOR)
+    exit_button_container = exit_button.get_rect(center = (WIDTH // 2, HEIGHT // 2 + 50))
+    screen.blit(exit_button, exit_button_container)
+
+    while True:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if exit_button_container.collidepoint(event.pos):
+                    sys.exit()
+        pygame.display.update()
+
+def draw_lose_screen(screen):
+    # Initialize fonts
+    title_font = pygame.font.Font(None, 200)
+    button_font = pygame.font.Font(None, 70)
+    
+    # Sets the screens background color to custom pallete
+    screen.fill(BACKGROUND_COLOR)
+
+    # Draw the title surface
+    title = title_font.render("YOU LOST!", 0, TEXT_COLOR)
+    title_container = title.get_rect(center = (WIDTH // 2, HEIGHT // 2 - 150))
+    screen.blit(title, title_container)
+
+    # Draw the "restart" button surface
+    restart_button = button_font.render("RESTART", 0, TEXT_COLOR)
+    restart_button_container = restart_button.get_rect(center = (WIDTH // 2, HEIGHT // 2 + 50))
+    screen.blit(restart_button, restart_button_container)
+
+    while True:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if restart_button_container.collidepoint(event.pos):
+                    return "easy"
+        pygame.display.update()
 
 # Main function where the screen and event handling is done
 if __name__ == "__main__":
@@ -59,7 +156,20 @@ if __name__ == "__main__":
     clock = pygame.time.Clock()
 
     # Welcome screen
-    draw_welcome_screen(screen)
+    difficulty = draw_welcome_screen(screen)
+
+    # Refresh's screen
+    screen.fill(BACKGROUND_COLOR)
+
+    # Game screen
+    status = draw_game_screen(screen, difficulty)
+
+    # End screen
+    if status == "win":
+        draw_win_screen(screen)
+
+    elif status == "lose":
+        draw_lose_screen(screen)
 
     while True:
 
