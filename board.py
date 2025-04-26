d5919d16fd3ca83d42b0351343818c9e373ab865
Mateo-Bedoya <mb291959@gmail.com>
2025-04-25 21:14:43 -0400

    new: added most methods, check over them, finish remaining

--- board.py
+++ board.py
@@ -37,8 +37,54 @@ class Board:
         for row in range(9):
             for col in range(9):
                 self.cells[row][col].draw()
-        #         x = col * cell_size
-        #         y = row * cell_size
-        #         pygame.draw.rect(self.screen, (200, 200, 200), (x, y, cell_size, cell_size), 1)
-        #
-        # pygame.display.update()
+
+        for i in range(10):
+            line_width = 3 if i % 3 == 0 else 1
+            pygame.draw.line(self.screen, (0, 0, 0), (i * cell_size, 0), (i * cell_size, self.height), line_width)
+            pygame.draw.line(self.screen, (0, 0, 0), (0, i * cell_size), (self.width, i * cell_size), line_width)
+
+    def select(self, row, col):
+        self.selected = (row, col)
+        return self.selected
+
+    def click(self, x, y):
+        pass
+
+    def clear(self):
+        if self.selected:
+            row, col = self.selected
+            self.cells[row][col].cell_value(0)
+
+    def sketch(self, value):
+        if self.selected:
+            row, col = self.selected
+            self.cells[row][col].sketched_value(value)
+
+    def place_number(self, value):
+        pass
+
+    def reset_to_original(self):
+        pass
+
+    def is_full(self):
+        for row in range(self.height):
+            for col in range(self.width):
+                if Cell.value == 0:
+                    return False
+        return True
+
+    def update_board(self):
+        for row in range(self.height):
+            for col in range(self.width):
+                self.board_values[row][col] = self.cells[row][col].value
+
+    def find_empty(self):
+
+        for row in range(self.height):
+            for col in range(self.width):
+                if self.cells[row][col].value == 0:
+                    return row, col
+        return None
+
+    def check_board(self):
+        pass
